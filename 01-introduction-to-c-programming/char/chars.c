f4869cca1fb1aff00a4cee30d59a29a51fc2d7de
Icaro Molinari <icaromolinari@gmail.com>
2026-08-03 19:03:03 -0300

    Add new chars exercise

---
+++ 01-introduction-to-c-programming/char/chars.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    char var1 = 65; // ASCII value for 'A'
+    char var2 = 66; // ASCII value for 'B'
+    printf("char 1: %c\n", var1); 
+    printf("char 2: %c\n", var2); 
+
+    return 0;
+}
